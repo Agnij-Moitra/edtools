2bba97276af23432b373645c5fba64c599c9f2be
Agnij Moitra <79319209+Agnij-Moitra@users.noreply.github.com>
2022-02-15 11:00:01 +0530

    format code

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def text_summerizer():
                 return render_template("Text-Out.html", summary=summary[0], keywords=summary[1])
             if ext == ".pdf":
                 text = get_pdf(file_path)
-        
+
                 try:
                     summary = get_summary(text)
                 except IndexError:
@@ -63,7 +63,7 @@ def text_summerizer():
                 return render_template("Text-Out.html", summary=summary[0], keywords=summary[1])
             if ext == ".docx":
                 text = get_docx(file_path)
-                
+
                 try:
                     summary = get_summary(text)
                 except IndexError:
@@ -73,7 +73,7 @@ def text_summerizer():
                 return render_template("Text-Out.html", summary=summary[0], keywords=summary[1])
             if ext == ".epub":
                 text = get_epub(file_path)
-                
+
                 try:
                     summary = get_summary(text)
                 except IndexError:
